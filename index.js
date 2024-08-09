524c6ded87f2cf42854f2a3f65eaa948aeee526a
siniourz <kazemisina45@gmail.com>
2024-08-10 00:11:34 +0300

    responsive fpr the signs 50-50

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function toggleMobileMenu(menu){
-    menu.classList.toggle('open');
+function toggleMobileMenu(element) {
+    element.classList.toggle("open");
+    const menu = document.querySelector('.mobile-menu');
+    const signDiv = document.querySelector('.m-four-sign');
+
+    if (element.classList.contains('open')) {
+        menu.style.display = 'flex';
+        signDiv.style.top = '150px'; // Adjust this value as needed to move the div further down
+    } else {
+        menu.style.display = 'none';
+        signDiv.style.top = '20px'; // Reset to the original position
+    }
 }
