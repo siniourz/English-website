e3168ab41b686ef5c8ffd5330a039de14a4b87e1
siniourz <kazemisina45@gmail.com>
2024-08-11 21:11:23 +0300

    responsive-part1-done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ function toggleMobileMenu(element) {
     element.classList.toggle("open");
     const menu = document.querySelector('.mobile-menu');
     const signDiv = document.querySelector('.m-four-sign');
+    const textPartsDiv = document.querySelector('.m-text-parts');
 
     if (element.classList.contains('open')) {
         menu.style.display = 'flex';
-        signDiv.style.top = '150px'; // Adjust this value as needed to move the div further down
+        signDiv.style.top = '150px'; // Adjust this value as needed to move the .m-four-sign div further down
+        textPartsDiv.style.marginTop = '150px'; // Adjust this value to move the .m-text-parts div further down
     } else {
         menu.style.display = 'none';
         signDiv.style.top = '20px'; // Reset to the original position
+        textPartsDiv.style.marginTop = '40px'; // Reset to the original position
     }
 }
