d16d88e96c1a57f5276b770b8aa63045b147c285
siniourz <kazemisina45@gmail.com>
2024-08-13 20:58:42 +0300

    new vocab for mobile

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function toggleMobileMenu(element) {
     element.classList.toggle("open");
     const menu = document.querySelector('.mobile-menu');
-    const signDiv = document.querySelector('.m-four-sign');
-    const textPartsDiv = document.querySelector('.m-text-parts');
+    const signDiv = document.querySelector('.m-vocab-div-for-img');
+    const textPartsDiv = document.querySelector('.m-vocab-box');
 
     if (element.classList.contains('open')) {
         menu.style.display = 'flex';
-        signDiv.style.top = '150px'; // Adjust this value as needed to move the .m-four-sign div further down
-        textPartsDiv.style.marginTop = '150px'; // Adjust this value to move the .m-text-parts div further down
+        signDiv.style.top = '350px'; // Adjust this value as needed to move the .m-four-sign div further down
+        textPartsDiv.style.marginTop = '250px'; // Adjust this value to move the .m-text-parts div further down
     } else {
         menu.style.display = 'none';
-        signDiv.style.top = '20px'; // Reset to the original position
-        textPartsDiv.style.marginTop = '40px'; // Reset to the original position
+        signDiv.style.top = '100px'; // Reset to the original position
+        textPartsDiv.style.marginTop = '0px'; // Reset to the original position
     }
 }
 
